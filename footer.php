ffca1975a0c4bed18c169cdd18aced970cbc4246
Nitika Yadav <145336566+207nitikayadav@users.noreply.github.com>
2024-09-06 18:15:52 +0530

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,27 @@
+<footer class="site-footer section-padding" id="contact" style="background-color: #c7ecee; padding:20px">
+<div class="container">
+    <div class="row">
+        <div class="col-lg-4 me-auto col-12">
+            <h5 class="mb-lg-3 mb-3">Timing</h5>
+            <p>9:00AM to 7:00PM </p>
+            <h5 class="mb-lg-3 mb-3">Email</h5>
+            <p>nitikayadav221@gmail.com </p>    
+            <h5 class="mb-lg-3 mb-3">Contact Number</h5>
+            <p>+91 7464834200</p>
+        </div>
+        <div class="col-lg-4 col-md-6 col-12 my-4 my-lg-0">
+                <h5 class="mb-lg-4 mb-3">Our Clinic</h5>                  
+                <p>Clement Town, Subhash Nagar</p>
+                <p>Dehradun - 248002</p>
+        </div>
+        
+        <div class="col-lg-3 col-md-6 col-12 ms-auto">
+                <h5 class="mb-lg-4 mb-2">Socials</h5>
+                <ul class="social-icon">
+                    <li><a href="" class="social-icon-link bi-facebook"></a></li>
+                    <li><a href="" class="social-icon-link bi-twitter"></a></li>
+                    <li><a href="" class="social-icon-link bi-instagram"></a></li>
+                </ul>    
+        </div>           
+    </div>
+</footer>
